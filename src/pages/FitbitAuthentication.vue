adca2b7ef698a99f15e2177751c59c33b83b571f
Khanh Nguyen <dagger_906@hotmail.com>
2024-10-11 16:59:31 +1100

    Next button logic

--- src/pages/FitbitAuthentication.vue
+++ src/pages/FitbitAuthentication.vue
@@ -33,7 +33,14 @@
         </svg>
         <svg v-if="fitbit" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon" class="h-4 ml-2"><path fill-rule="evenodd" stroke="currentColor" stroke-width="1" d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z" clip-rule="evenodd"></path></svg>
       </button>
-      <button @click="goToNext()" :disabled="loading" class="button-secondary">Skip</button>
+      <button @click="goToNext()" :disabled="loading" :class="{ 'button-secondary': !fitbit }" class="inline-flex items-center place-content-center">
+        <svg v-if="loading" class="animate-spin h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+          <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+          <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+        </svg>
+        <span v-else-if="!fitbit">Skip</span>
+        <span v-else>Next →</span>
+      </button>
       <button @click="getSleepData('2024-09-30', '2024-10-06')">Get sleep data</button>
       <ul class="mt-5 px-4">
         <li v-for ="s in sleepData" :key="s">
@@ -153,7 +160,9 @@ export default {
       return Math.floor(minutes / 60).toString() + 'hrs ' + Math.round(minutes / 60 % 1 * 60).toString() + 'mins'
     },
     goToNext() {
-
+      this.loading = true
+      // this.$router.push('/nextPage')
+      // this.loading = false
     },
   },
 }
